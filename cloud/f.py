fa1e066d0cefdf44831d658d6adb03a60bc02fd4
musyoku <musyoku@users.noreply.github.com>
2017-04-19 15:22:34 +0900

    :v:

--- cloud/f.py
+++ cloud/f.py
@@ -76,41 +76,51 @@ def main(args):
 	# ベクトルを取得
 	doc_vectors = np.asarray(cstm.get_doc_vectors(), dtype=np.float32)
 	assert args.doc_id < doc_vectors.shape[0]
-	doc_vector = doc_vectors[args.doc_id]
 
-	# 各単語についてfを計算
-	dic = {}
+	doc_ids = []
+	if args.doc_id is None:
+		num_docs = cstm.get_num_documents()
+		for doc_id in xrange(num_docs):
+			doc_ids.append(doc_id)
+	else:
+		doc_ids.append(args.doc_id)
+
+	# 文書ごとに生成
 	words = cstm.get_words()
-	for meta in words:
-		word = meta[1]
-		count = meta[2]
-		if count < args.min_occurence:
-			continue
-		word_vector = np.asarray(meta[3], dtype=np.float32)
-		f = np.inner(word_vector, doc_vector)
-		dic[word] = f
-	dic = sorted(dic.items(), key=lambda x: -x[1])	# sortedが昇順なのでマイナスを掛ける
+	for doc_id in doc_ids:
+		doc_vector = doc_vectors[doc_id]
+		# 各単語についてfを計算
+		dic = {}
+		for meta in words:
+			word = meta[1]
+			count = meta[2]
+			if count < args.min_occurence:
+				continue
+			word_vector = np.asarray(meta[3], dtype=np.float32)
+			f = np.inner(word_vector, doc_vector)
+			dic[word] = f
+		dic = sorted(dic.items(), key=lambda x: -x[1])	# sortedが昇順なのでマイナスを掛ける
 
-	max_count = min(args.max_num_word, len(dic))
-	dic = dict(dic[:max_count])
+		max_count = min(args.max_num_word, len(dic))
+		dic = dict(dic[:max_count])
 
-	wordcloud = WordCloud(
-		background_color="white",
-		font_path=args.font_path, 
-		width=args.width, 
-		height=args.height, 
-		max_words=max_count, 
-		max_font_size=args.max_font_size).generate_from_frequencies(dic)
-	color_funcs = [None, color_func_1, color_func_2, color_func_3, color_func_4]
-	color_func = color_funcs[args.color]
-	wordcloud.recolor(color_func=color_func)
-	wordcloud.to_file("{}/cloud_f_{}.png".format(args.output_dir, args.doc_id))
+		wordcloud = WordCloud(
+			background_color="white",
+			font_path=args.font_path, 
+			width=args.width, 
+			height=args.height, 
+			max_words=max_count, 
+			max_font_size=args.max_font_size).generate_from_frequencies(dic)
+		color_funcs = [None, color_func_1, color_func_2, color_func_3, color_func_4]
+		color_func = color_funcs[args.color]
+		wordcloud.recolor(color_func=color_func)
+		wordcloud.to_file("{}/cloud_f_{}.png".format(args.output_dir, doc_id))
 				
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-m", "--model-filename", type=str, default="../model/cstm.model")
 	parser.add_argument("-o", "--output-dir", type=str, default="../out")
-	parser.add_argument("-doc", "--doc-id", type=int, default=0, help="文書ID")
+	parser.add_argument("-doc", "--doc-id", type=int, default=None, help="文書ID")
 	parser.add_argument("--width", type=int, default=1440, help="クラウドの幅.")
 	parser.add_argument("--height", type=int, default=1080, help="クラウドの高さ.")
 	parser.add_argument("--color", type=int, default=1, help="クラウドのcolor_func番号.")
